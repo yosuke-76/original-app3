be4c81cf524579f82a284421ebaad5e93276987f
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-08-27 23:31:38 +0900

    投稿の編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,19 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
