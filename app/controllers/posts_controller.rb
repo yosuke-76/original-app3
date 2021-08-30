2deb05e67be8d72d83ca8b9a3da4ffb6706aa913
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-08-30 21:26:25 +0900

    Active Storageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:name, :text).merge(user_id: current_user.id)
+    params.require(:post).permit(:name, :text, :image).merge(user_id: current_user.id)
   end
 
   def set_post
